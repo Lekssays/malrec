ee0eeb2c23ff7341cbfb2dce17a49b086ceb35d9
Ahmed Lekssays <ahmed@lekssays.com>
2021-05-25 18:07:00 +0200

    make peers id selection modular

--- caliper/workloads/queryBackup.js
+++ caliper/workloads/queryBackup.js
@@ -2,6 +2,8 @@
 
 const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
 
+const peers = ['peer0.org1.example.com', 'peer0.org2.example.com', 'peer0.org3.example.com'];
+
 class MyWorkload extends WorkloadModuleBase {
     constructor() {
         super();
@@ -11,13 +13,14 @@ class MyWorkload extends WorkloadModuleBase {
         await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
 
         for (let i=0; i<this.roundArguments.backups; i++) {
-            const backupID = `${this.workerIndex}_${i}`;
+            const backupID = `BACKUP_${this.workerIndex}_${i}`;
+            const peerId = Math.floor(Math.random() * (peers.length - 1));
             console.log(`Worker ${this.workerIndex}: Creating backup ${backupID}`);
             const request = {
                 contractId: this.roundArguments.contractId,
                 contractFunction: 'CreateBackup',
                 invokerIdentity: 'peer0.org1.example.com',
-                contractArguments: [backupID,'QmdXYvmSEXrA9EoFBDQJRqrYiBLF6UB5o5M3pBSM4xJMuH'],
+                contractArguments: [backupID, peers[peerId], 'QmdXYvmSEXrA9EoFBDQJRqrYiBLF6UB5o5M3pBSM4xJMuH'],
                 readOnly: false
             };
 
@@ -31,7 +34,7 @@ class MyWorkload extends WorkloadModuleBase {
             contractId: this.roundArguments.contractId,
             contractFunction: 'QueryBackup',
             invokerIdentity: 'peer0.org1.example.com',
-            contractArguments: [`${this.workerIndex}_${randomId}`],
+            contractArguments: [`BACKUP_${this.workerIndex}_${randomId}`],
             readOnly: true
         };
 
@@ -40,7 +43,7 @@ class MyWorkload extends WorkloadModuleBase {
     
     async cleanupWorkloadModule() {
         for (let i=0; i<this.roundArguments.backups; i++) {
-            const backupID = `${this.workerIndex}_${i}`;
+            const backupID = `BACKUP_${this.workerIndex}_${i}`;
             console.log(`Worker ${this.workerIndex}: Deleting backup ${backupID}`);
             const request = {
                 contractId: this.roundArguments.contractId,
