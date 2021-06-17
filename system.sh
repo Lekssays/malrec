25b532a1361eb68d3e1f499d3b86e96595583705
Giorgia <giorgia.sir95@outlook.com>
2021-06-17 14:27:50 +0200

    Removed '&' from row 184, and add networkDown method

--- system.sh
+++ system.sh
@@ -60,6 +60,10 @@ function restartNetwork() {
   docker volume rm $(docker volume ls)  || true
 }
 
+function networkDown() {
+  clearNetwork
+}
+
 function clearNetwork {
   docker rm -f $(docker ps -aq) || true
   docker rmi -f $(docker images -a -q) || true
@@ -177,7 +181,7 @@ function startBackupMonitoring() {
     for ((peerId=0; peerId<$PEERS; peerId++));
     do
       echo "Starting backup engine on peer$peerId.org$orgId.example.com..."
-      docker exec -d peer$peerId.org$orgId.example.com /bin/sh -c "/bin/sh /core/monitor.sh &"
+      docker exec -d peer$peerId.org$orgId.example.com /bin/sh -c "/bin/sh /core/monitor.sh"
     done
   done
 }
